bb1c4639b4fea84fe0f390bd78f5a261b550d484
kushieda minori <kushieda@zetsuboushita.net>
2020-11-29 13:22:13 -0500

    add ABB day calculation and minor bug fix

--- discord-webhook-ABB.sh
+++ discord-webhook-ABB.sh
@@ -31,19 +31,41 @@ if [ -z $url ]; then
     exit 1
 fi
 
+isPOSIXDate() {
+    date -j -f "%a" "Sat" &> /dev/null
+    return $?
+}
+
 # calculate the end of the ABB
 endDateTime() {
     local ret
-    date -j -f "%a" "Sat" &> /dev/null
-    if [ $? -eq 0 ]; then
+    if isPOSIXDate; then
         # we are on BSD
-        ret="$(TZ=Asia/Tokyo date -jv +5d -f '%Y-%m-%d' "$1-$2-$3" +'%Y %m %d' )"
+        ret="$(TZ=Asia/Tokyo date -jv +5d -f '%Y-%m-%d' "$startY-$startM-$startD" +'%Y %m %d' )"
     else
         # we are on linux
-        ret="$(TZ=Asia/Tokyo date --date "$1-$2-$3+5 day" +'%Y %m %d' )"
+        ret="$(TZ=Asia/Tokyo date --date "$startY-$startM-$startD+5 day" +'%Y %m %d' )"
     fi
     echo $ret
     return 0
+}
+
+getABBDay() {
+    local DAY
+    local ld
+    for DAY in 0 1 2 3 4 5 ; do
+        if isPOSIXDate; then
+            # we are on BSD
+            ld="$(TZ=Asia/Tokyo date -jv +${DAY}d -f '%Y-%m-%d' "$startY-$startM-$startD" +'%Y%m%d' )"
+        else
+            # we are on linux
+            ld="$(TZ=Asia/Tokyo date --date "$startY-$startM-$startD+${DAY} day" +'%Y%m%d' )"
+        fi
+        if [[ "$ld" -eq "$Y$M$D" ]] ; then
+            echo $(( $DAY + 1))
+            return 0
+        fi
+    done
 }
 
 # get the current JST date
@@ -51,8 +73,9 @@ currentDatetime="$(TZ=Asia/Tokyo date +'%Y %m %d %H %M')"
 #echo $currentDatetime
 
 read Y M D h m <<< "$currentDatetime"
-read eY eM eD <<< "$(endDateTime $startY $startM $startD)"
-echo "End: $eY $eM $eD"
+read eY eM eD <<< "$(endDateTime)"
+#echo "End: $eY $eM $eD"
+#echo "ABB Day: $(getABBDay)"
 
 if [[ "$Y$M$D" -lt "$startY$startM$startD" || "$Y$M$D" -gt "$eY$eM$eD" ]]; then
     # current date is not a configured ABB range
@@ -62,30 +85,41 @@ fi
 # 08 - 09, 12 - 13, 19 - 20, 22 - 23 (in JST)
 # get upcoming Round number
 
-timeTillNext=$((60 - $m))
+# strip 0 for math since leading 0's indicate an octal number.
+# if the minute happens to be '09', this fails since '09' is
+# not a valid octal number
+nm=${m##+(0)}
+nm=${nm:-0}
+# same as above, but for hours.
+nh=${h##+(0)}
+nh=${nh:-0}
 
-if (( $h == 7 || ($h == 8 && $m==0) )); then
+timeTillNext=$((60 - $nm))
+
+if (( $nh == 7 || ($nh == 8 && $nm == 0) )); then
     round="1"
-elif (( $h == 11 || ($h == 12 && $m==0) )); then
+elif (( $nh == 11 || ($nh == 12 && $nm == 0) )); then
     round="2"
-elif (( $h == 18 || ($h == 19 && $m==0) )); then
+elif (( $nh == 18 || ($nh == 19 && $nm == 0) )); then
     round="3"
-elif (( $h == 21 || ($h == 22 && $m==0) )); then
+elif (( $nh == 21 || ($nh == 22 && $nm == 0) )); then
     round="4"
 else
     # not close enough to the next round.
     exit
 fi
 
+ABB_DAY=$(getABBDay)
+
 # no R1 day 1
-if (( $startD == $D && $round == 1 )); then
+if (( $ABB_DAY == 1 && $round == 1 )); then
     exit
 fi
 
-if (( $timeTillNext == 60 && ($h == 8 || $h == 12 || $h == 19 || $h == 22) )); then
-    message="ABB Round $round START! $4"
+if (( $timeTillNext == 60 && ($nh == 8 || $nh == 12 || $nh == 19 || $nh == 22) )); then
+    message="ABB Day $ABB_DAY Round $round START! $4"
 else
-    message="ABB Round $round in $timeTillNext minutes! $4"
+    message="ABB Day $ABB_DAY Round $round in $timeTillNext minutes! $4"
 fi
 
 curl -sS -H 'Content-Type: application/json' -X POST -d "{\"content\": \"${message}\"}" "$url"
