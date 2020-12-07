b7de0c1027ef4e02c7abcff82fadc3ba4889df81
kushieda minori <kushieda@zetsuboushita.net>
2020-12-06 20:25:34 -0500

    add more alerts for events

--- discord-webhook-Event.sh
+++ discord-webhook-Event.sh
@@ -19,29 +19,44 @@ isPOSIXDate() {
 }
 
 # calculate the time 1 hour in the future. this is to give "End of event" notices
-oneHourLater() {
+xHourLater() {
     local ret
     if isPOSIXDate; then
         # we are on BSD
-        ret="$(TZ=Asia/Tokyo date -jv +1H +'%Y%m%d%H' )"
+        ret="$(TZ=Asia/Tokyo date -jv +${1}H +'%Y%m%d%H' )"
     else
         # we are on linux
-        ret="$(TZ=Asia/Tokyo date --date "+1 hour" +'%Y%m%d%H' )"
+        ret="$(TZ=Asia/Tokyo date --date "+${1} hour" +'%Y%m%d%H' )"
     fi
     echo $ret
     return 0
 }
 
-oneHrLater=$(oneHourLater)
+oneHrLater=$(xHourLater 1)
+twoHrLater=$(xHourLater 2)
+oneDayLater=$(xHourLater 24)
+twoDayLater=$(xHourLater 48)
 
 if [[ "$event_start" -eq "${currentHour}" ]]; then
     message="${event_name} starts now"
+    curl -sS -H 'Content-Type: application/json' -X POST -d "{\"content\": \"${message}\"}" "${url}"
+elif [[ "$event_start" -eq "${oneHrLater}" ]]; then
+    message="${event_name} starts in 1 hour"
     curl -sS -H 'Content-Type: application/json' -X POST -d "{\"content\": \"${message}\"}" "${url}"
 elif [[ "$event_end" -eq "${currentHour}" ]]; then
     message="${event_name} ended"
     curl -sS -H 'Content-Type: application/json' -X POST -d "{\"content\": \"${message}\"}" "${url}"
 elif [[ "$event_end" -eq "${oneHrLater}" ]]; then
     message="${event_name} ending in 1 hour"
+    curl -sS -H 'Content-Type: application/json' -X POST -d "{\"content\": \"${message}\"}" "${url}"
+elif [[ "$event_end" -eq "${twoHrLater}" ]]; then
+    message="${event_name} ending in 2 hours"
+    curl -sS -H 'Content-Type: application/json' -X POST -d "{\"content\": \"${message}\"}" "${url}"
+elif [[ "$event_end" -eq "${oneDayLater}" ]]; then
+    message="${event_name} ending in 24 hours"
+    curl -sS -H 'Content-Type: application/json' -X POST -d "{\"content\": \"${message}\"}" "${url}"
+elif [[ "$event_end" -eq "${twoDayLater}" ]]; then
+    message="${event_name} ending in 48 hours"
     curl -sS -H 'Content-Type: application/json' -X POST -d "{\"content\": \"${message}\"}" "${url}"
 fi
 
